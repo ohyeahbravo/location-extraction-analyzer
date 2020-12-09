7cb3c65dbbdafca812b746fbbfaa85c661f5d5db
Hiyeon <ohyeahbravo@gmail.com>
2020-12-09 18:04:12 +0100

    deleted deployment option as the page is not static

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,6 @@
 // vue.config.js
 
 module.exports = {
-    publicPath:
-        process.env.NODE_ENV === 'production'
-            ? '/location-extraction-analyzer'
-            : '/',
-    outputDir: 'docs',
     css: {
         loaderOptions: {
             sass: {
